2aba87f2f5749789b91b7fa116d58c08bfdf1e73
Matt Walker <walkermatt@longwayaround.org.uk>
2013-05-22 15:51:30 +0100

    Add habitat layers and charts

--- readme.md
+++ readme.md
@@ -8,3 +8,10 @@ All data is displayed in Spherical Mercator, to enable MapServer to output data 
 
 `<900913> +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs`
 
+Notes
+-----
+
+To create the OS raster tile index:
+
+`cd ~/Downloads/naturalresourceswales/FMM/Data_for_Contractor/BaseMapping`
+`gdaltindex ./OS250k/os250k.shp ./OS250k/*.TIF`
--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -3,4 +3,12 @@
 # Assumes that the current directory is the root of the FishMap data directory
 # containing FMM_Data, HabitatLayers directories etc.
 
+# Project Area
 ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' Project_Area/PROJECT_AREA.TAB -nln project_area -s_srs "EPSG:27700" -a_srs "EPSG:27700"
+
+# Habitat
+ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' "HabitatLayers/Intertidal/INTERTIDAL Fisheries Habitats.TAB" -nln intertidal_habitats -s_srs "EPSG:27700" -a_srs "EPSG:27700"
+
+ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' "HabitatLayers/Subtidal/SUBTIDAL Fisheries Habitats.tab" -nln subtidal_habitats -s_srs "EPSG:27700" -a_srs "EPSG:27700"
+
+ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' "HabitatLayers/Subtidal/confidencelayer.tab" -nln subtidal_habitats_confidence -s_srs "EPSG:27700" -a_srs "EPSG:27700"
