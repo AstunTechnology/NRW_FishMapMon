0554776db5be7db029187a0db21b0299dcfef4cd
Matt Walker <walkermatt@longwayaround.org.uk>
2013-05-23 15:14:09 +0100

    Use new more accurate project area

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -4,7 +4,7 @@
 # containing FMM_Data, HabitatLayers directories etc.
 
 # Project Area
-ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' Project_Area/PROJECT_AREA.TAB -nln project_area -s_srs "EPSG:27700" -a_srs "EPSG:27700"
+ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' Project_Area/Project_Area_Boundaryline.TAB -nln project_area -s_srs "EPSG:27700" -a_srs "EPSG:27700"
 
 # Habitat
 ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=postgres password=postgres' "HabitatLayers/Intertidal/INTERTIDAL Fisheries Habitats.TAB" -nln intertidal_habitats -s_srs "EPSG:27700" -a_srs "EPSG:27700"
