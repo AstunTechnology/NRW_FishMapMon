2a62f608a945d702345675bc6b8169bec7900a9c
Matt Walker <walkermatt@longwayaround.org.uk>
2013-05-29 14:54:01 +0100

    Tidy up indentation

--- webapp/static/js/LegendPanel.js
+++ webapp/static/js/LegendPanel.js
@@ -1,69 +1,69 @@
 OpenLayers.Control.LegendPanel = OpenLayers.Class(OpenLayers.Control, {
 
-/**
- * Constructor: OpenLayers.Control.LegendPanel
- *
- * Parameters:
- * options - {Object} Options for control.
- */
-initialize: function(options) {
-    this.layer = options.layer;
-    OpenLayers.Control.prototype.initialize.apply(this, arguments);
-},
+    /**
+    * Constructor: OpenLayers.Control.LegendPanel
+    *
+    * Parameters:
+    * options - {Object} Options for control.
+    */
+    initialize: function(options) {
+        this.layer = options.layer;
+        OpenLayers.Control.prototype.initialize.apply(this, arguments);
+    },
 
-showLayers: function(layers) {
-    var tmpl = jQuery('#legendTmpl').html();
-    jQuery('.content', this.legendDiv).empty();
-    for (var n = 0, lyr; n < layers.length; n++) {
-        lyr = layers[n];
-        var legendElm = jQuery.mustache(tmpl, {
-            "url": FISH_MAP.WMS_OVERLAY_URL + "&LAYER=" + lyr + "&VERSION=1.1.1&SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png&SLD=" + encodeURIComponent(FISH_MAP.SLD_URL + lyr),
-            "name": function() {
-                return window.FISH_MAP.text[lyr];
-            }
+    showLayers: function(layers) {
+        var tmpl = jQuery('#legendTmpl').html();
+        jQuery('.content', this.legendDiv).empty();
+        for (var n = 0, lyr; n < layers.length; n++) {
+            lyr = layers[n];
+            var legendElm = jQuery.mustache(tmpl, {
+                "url": FISH_MAP.WMS_OVERLAY_URL + "&LAYER=" + lyr + "&VERSION=1.1.1&SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png&SLD=" + encodeURIComponent(FISH_MAP.SLD_URL + lyr),
+                "name": function() {
+                    return window.FISH_MAP.text[lyr];
+                }
+            });
+            jQuery('.content', this.legendDiv).append(legendElm);
+        }
+    },
+
+    /**
+    * Method: draw
+    * Initialize control.
+    *
+    * Returns:
+    * {DOMElement} A reference to the DIV DOMElement containing the control
+    */
+    draw: function() {
+        OpenLayers.Control.prototype.draw.apply(this, arguments);
+
+        this.legendDiv = document.createElement('div');
+        jQuery(this.legendDiv).addClass("legend").addClass("open");
+        this.legendDiv.innerHTML = "<h2>Legend</h2><div class='content'></div>";
+        this.div.appendChild(this.legendDiv);
+
+        jQuery('.legend', this.div).children().prev().click(function(evt) {
+            jQuery(this).siblings().toggle();
+            jQuery(this).parent().toggleClass('open')
         });
-        jQuery('.content', this.legendDiv).append(legendElm);
-    }
-},
 
-/**
- * Method: draw
- * Initialize control.
- *
- * Returns:
- * {DOMElement} A reference to the DIV DOMElement containing the control
- */
-draw: function() {
-    OpenLayers.Control.prototype.draw.apply(this, arguments);
+        this.events = new OpenLayers.Events(this, this.div, null, true);
 
-    this.legendDiv = document.createElement('div');
-    jQuery(this.legendDiv).addClass("legend").addClass("open");
-    this.legendDiv.innerHTML = "<h2>Legend</h2><div class='content'></div>";
-    this.div.appendChild(this.legendDiv);
+        var stopEvt = function(evt) {
+            OpenLayers.Event.stop(evt, true);
+        };
 
-    jQuery('.legend', this.div).children().prev().click(function(evt) {
-        jQuery(this).siblings().toggle();
-        jQuery(this).parent().toggleClass('open')
-    });
+        this.events.on({
+            "mousedown": stopEvt,
+            "mousemove": stopEvt,
+            "mouseup": stopEvt,
+            "click": stopEvt,
+            "mouseout": stopEvt,
+            "dblclick": stopEvt,
+            scope: this
+        });
 
-    this.events = new OpenLayers.Events(this, this.div, null, true);
+        return this.div;
+    },
 
-    var stopEvt = function(evt) {
-        OpenLayers.Event.stop(evt, true);
-    };
-
-    this.events.on({
-        "mousedown": stopEvt,
-        "mousemove": stopEvt,
-        "mouseup": stopEvt,
-        "click": stopEvt,
-        "mouseout": stopEvt,
-        "dblclick": stopEvt,
-        scope: this
-    });
-
-    return this.div;
-},
-
-CLASS_NAME: "OpenLayers.Control.LegendPanel"
+    CLASS_NAME: "OpenLayers.Control.LegendPanel"
 });
