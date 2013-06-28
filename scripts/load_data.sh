70c0d8fdfb428e010adb43ba4c7e653c8dd2756d
Matt Walker <walkermatt@longwayaround.org.uk>
2013-06-28 10:02:31 +0100

    Add mask layer
    The mask and project_area layers are not included in all overlay WMS
    requests by the client in the appropriate position in the layer stack to
    ensure they are draw at the expected position

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -9,6 +9,9 @@ set -o verbose
 # Project Area
 ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=fishmap_webapp password=<password>' Project_Area/Project_Area_Boundaryline.TAB -nln project_area -s_srs "EPSG:27700" -a_srs "EPSG:27700"
 
+# Mask
+ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=fishmap_webapp password=<password>' BaseMapping/12nm_Mask/12nm_Mask.TAB -nln mask -s_srs "EPSG:27700" -a_srs "EPSG:27700"
+
 # High Water (used with No Map base map)
 ogr2ogr -overwrite -skipfailures -f PostgreSQL PG:'dbname=fishmap active_schema=public host=localhost user=fishmap_webapp password=<password>' BaseMapping/high_water_wales.TAB -nln high_water_wales -s_srs "EPSG:27700" -a_srs "EPSG:27700"
 
