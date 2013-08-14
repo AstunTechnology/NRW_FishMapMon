339ec933d6eeffb6e9d1743da0e2c66d37dc6e10
Matt Walker <walkermatt@longwayaround.org.uk>
2013-08-14 13:10:45 +0100

    Import new Habitat data and increase extents to all of Wales

--- webapp/static/js/map.js
+++ webapp/static/js/map.js
@@ -35,7 +35,7 @@
         units: "m",
         resolutions: [2.5, 5, 10, 25, 50, 100, 150],
         maxExtent: new OpenLayers.Bounds(-3276800,-3276800,3276800,3276800),
-        restrictedExtent: new OpenLayers.Bounds(120000, 298000, 364000, 445000),
+        restrictedExtent: new OpenLayers.Bounds(120000, 130000, 375000, 445000),
         controls: controls
     });
 
